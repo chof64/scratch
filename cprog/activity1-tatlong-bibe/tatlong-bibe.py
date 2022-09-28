be9a8847cb9feb01dbb447dd7363032034541908
Chad Fernandez <chadfernandez@duck.com>
2022-09-28 18:02:42 +0800

    tatlong-bibe output

---
+++ cprog/activity1-tatlong-bibe/tatlong-bibe.py
@@ -0,0 +1,19 @@
+"""
+    ACTIVITY 1
+
+    CHAD HACKETT O. FERNANDEZ
+    BS COMSCI 1-A
+"""
+
+print(
+    """
+May tatlong bibe akong nakita.
+     Mataba, mapayat mga bibe,
+         Ngunit ang may pakpak
+               Sa likod nagiisa
+Siya and lider na nagsabi ng
+     'Kwak, kwak!'
+          "Kwak kwak kwak"(2x)
+Siya ang lider na nagsabi ng "kwak kwak!" 
+"""
+)
